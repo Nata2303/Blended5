d01aa90933c7950d127604d751ac38defa481953
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-03-02 15:46:24 +0000

    Deploying to gh-pages from @ Nata2303/Blended5@089d24e0c4fa9f1326655f265a981b8e40cd8a66 🚀

---
+++ assets/room-page-f8010257.js
@@ -0,0 +1,1 @@
+import"./main-5d7e2f69.js";new Swiper(".slider-img",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},keyboard:{enabled:!0,onlyInViewport:!0},preloadImages:!0,slidesPerView:1.5,simulateTouch:!0,touchRatio:3,grabCursor:!0,spaceBetween:20,initialSlide:9,breakpoints:{768:{slidesPerView:1.5},1440:{slidesPerView:1.5}}});
