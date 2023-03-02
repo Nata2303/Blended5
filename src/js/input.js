2d99ad8b90ac7a6c062e69ac28efb30f99af2c39
Kostia31 <kostyapodnebesnuy@gmail.com>
2023-03-02 17:37:36 +0200

    changes

--- src/js/input.js
+++ src/js/input.js
@@ -1,6 +1,6 @@
-flatpickr('input[type=datetime]', {
-  datetime: {
-    enableTime: false,
-    dateFormat: 'Y/m/d H:i',
-  },
-});
+// flatpickr('input[type=datetime]', {
+//   datetime: {
+//     enableTime: false,
+//     dateFormat: 'Y/m/d H:i',
+//   },
+// });
